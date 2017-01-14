12028334e26d9ff61c4dfa0a01567cab52a0304e
Raphael MIMRAN <mims@MacBook-Air-de-Raphael.local>
2017-01-14 16:39:44 +0100

    ajout des pages connexions

---
+++ connexion-non-securise.php
@@ -0,0 +1,39 @@
+<?php 
+
+if(!empty($_POST['login']) && !empty($_POST['mdp'])){
+	$message = 'Bonjour ';
+
+	$login = $_POST['login'];
+	$mdp = $_POST['mdp'];
+
+	$filename = 'id.txt';
+	$file = fopen($filename, 'a+');
+	fwrite($file, 'Login : '.$login.' mdp : '.$mdp.'  |  |  |  ');
+	$contents = file_get_contents($filename);
+	fclose($file);
+} 
+
+
+ ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<meta charset="UTF-8">
+</head>
+<body>
+	
+	<p><?= (!empty($message) ? $message.' '.$login : '')?></p>
+	
+	<form method="POST" id="form">
+		<label for="login">login : </label><input type="text" name="login">
+		<br/>
+		<label for="mdp">mdp : </label><input type="text" name="mdp">
+		<br/>
+		<input type="submit" value="ok">
+	</form>
+	
+
+</body>
+</html>
